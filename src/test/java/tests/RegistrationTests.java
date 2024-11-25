ebc602ce11ce897813e0599f6fd8220150d8b439
User <inasaz2020@gmail.com>
2024-11-25 20:39:57 +0200

    lesson5

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class RegistrationTests {
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class RegistrationTests extends ApplicationManager {
+
+    @Test
+    public void registrationPositiveTest() {
+        new HomePage(getDriver()).clicBtnLoginHeader();
+        new LoginPage(getDriver()).typeLoginForm("my_email", "password");
+
+    }
+
+
 }
